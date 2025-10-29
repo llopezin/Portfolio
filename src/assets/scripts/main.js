aec03ebe70d76a11ad1689dc240acdff7bb9cb57
Laura <lauralopezinfantes@gmail.com>
2025-10-29 22:47:30 +0100

    feat: content update

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -4,15 +4,12 @@
  */
 import "scroll-behavior-polyfill";
 
-/**
- * Write any other JavaScript below
- */
 
 // Observe intersection of animated items
 const addIntersectedClass = () => {
     const animatedItems = document.querySelectorAll(".animate");
     const intersectionConfig = {
-        threshold: 0.6,
+        threshold: 0.3,
     };
 
     const observer = new IntersectionObserver(
@@ -32,28 +29,56 @@ const addIntersectedClass = () => {
 }
 
 
-//Utils
+// Animate background color on section change
+// let initialDistanceToTop = 0;
 
-//Poll for the existence of an HTML element 
-function pollForElement(selector, callback, interval = 100, timeout = 10000) {
-    const startTime = Date.now(); 
+function onScroll(target, initialDistanceToTop, reverse) {
+    const background = document.querySelector(".background");
+    const targetDistanceToTop = target.getBoundingClientRect().top;
+    const distanceRatio = targetDistanceToTop / initialDistanceToTop;
 
-    function check() {
-        const element = document.querySelector(selector)    ; 
-        const elapsedTime = Date.now() - startTime; 
+    reverse
+        ? background.style.opacity = distanceRatio
+        : background.style.opacity = 1 - distanceRatio;
 
-        if (element) {
-            callback(element); 
-        } else if (elapsedTime < timeout) { 
-            setTimeout(check, interval); 
-        } else {
-            callback(null); 
-        }
-    }
-
-    check(); 
+    if(targetDistanceToTop <= 0) window.onscroll = null
 }
 
+function changeBackgroundColor() {
+    const sections = document.querySelectorAll("section, header");
+    const header = document.querySelector("header");
+    const background = document.querySelector(".background");
 
-//Initialize
-pollForElement('.animate', addIntersectedClass);
+    const intersectionConfig = {
+        threshold: 0.1,
+    };
+
+    const observer = new IntersectionObserver(
+        (entries) => {
+            entries.forEach(({isIntersecting, target}) => {
+                const initialDistanceToTop = target.getBoundingClientRect().top;
+                
+                if (isIntersecting) {
+                    if(target.classList.contains("section--primary")) background.classList = "background background--primary";
+                    if(target.classList.contains("section--secondary")) background.classList = "background background--secondary";
+                    if(target === header) {background.style.opacity = 1; return};
+                    
+                    const reverse = target.classList.contains("section--neutral")
+
+                    window.onscroll = ()=>onScroll(target, initialDistanceToTop, reverse);
+                    observer.unobserve(target);
+                }
+            });
+        },
+        intersectionConfig
+    );
+
+    sections.forEach((item) => {
+        observer.observe(item);
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    addIntersectedClass();
+    // changeBackgroundColor();
+});
